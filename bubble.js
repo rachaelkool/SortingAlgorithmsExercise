3cb1f38235a7127adad977f2016121703ee5289c
Rachael Shipes <Rachael@Rachaels-MacBook-Pro-2.local>
2023-02-10 14:08:06 -0500

    completed exercise

---
+++ bubble.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        let swapped = false;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+
+        return arr;
+    }
+}
+
+module.exports = bubbleSort;
